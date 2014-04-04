3a57d99cea5b3542c446ff5ed426e3f4e7a73b1c
Jesse Pollak <jpollak92@gmail.com>
2014-04-03 19:16:56 -0700

    adds better responsiveness

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,36 @@ var gulp = require('gulp'),
   liveReloadServer = require('tiny-lr'),
   livereload = require('gulp-livereload'),
   gutil = require('gulp-util'),
+  ejs = require('gulp-ejs'),
   express = require('express');
-
 
 var lr = liveReloadServer();
 
 gulp.task('scripts', function() {
-    gulp.src(['src/coffee/**/*.coffee'])
-        .pipe(coffee().on('error', function(err){
-            gutil.log(gutil.colors.red(err))
-        }))
-        .pipe(gulp.dest('build/js'))
-        .pipe(livereload(lr));
+  return gulp.src(['src/coffee/**/*.coffee'])
+    .pipe(coffee().on('error', function(err){
+        gutil.log(gutil.colors.red(err))
+    }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(livereload(lr));
 });
 
 gulp.task('styles', function() {
-    gulp.src(['src/sass/**/*.scss'])
-        .pipe(sass({errLogToConsole: true}).on('error', function(err){
-            gutil.log(gutil.colors.red('Error in SASS syntax'), err);
-        }))
-        .pipe(gulp.dest('build/css'))
-        .pipe(livereload(lr));
+  return gulp.src(['src/sass/**/*.scss'])
+    .pipe(sass({errLogToConsole: true}).on('error', function(err){
+        gutil.log(gutil.colors.red('Error in SASS syntax'), err);
+    }))
+    .pipe(gulp.dest('build/css'))
+    .pipe(livereload(lr));
 });
+
+gulp.task('html', function() {
+  var companies = require('./src/data/companies').sort(function(a, b) { a.name - b.name });
+  return gulp.src(['src/html/index.html'])
+    .pipe(ejs({companies: companies}))
+    .pipe(gulp.dest('.'))
+    .pipe(livereload(lr));
+})
 
 function startExpress() {
   var app = express();
@@ -38,9 +46,10 @@ function startLiveReload() {
   lr.listen(35729);
 }
 
-gulp.task('default', ['scripts', 'styles'], function() {
+gulp.task('default', ['scripts', 'styles', 'html'], function() {
   gulp.watch('src/coffee/**', ['scripts']);
   gulp.watch('src/sass/**', ['styles']);
+  gulp.watch(['src/html/**', 'src/data/**'], ['html']);
 
   startExpress();
   startLiveReload();
---
+++ src/data/companies.js
@@ -0,0 +1,58 @@
+exports = module.exports = [
+    {
+        name: 'BabyList',
+        url: 'http://babyli.st'
+    },
+    {
+        name: 'Building Robotics',
+        url: 'http://buildingrobotics.com'
+    },
+    {
+        name: 'Clef',
+        url: 'https://getclef.com'
+    },
+    {
+        name: 'Immunity Project',
+        url: 'http://immunityproject.org'
+    },
+    {
+        name: 'Kato',
+        url: 'http://kato.im'
+    },
+    {
+        name: 'Livescribe',
+        url: 'http://livescribe.com'
+    },
+    {
+        name: 'Mosaic',
+        url: 'http://joinmosaic.com'
+    },
+    {
+        name: 'Mayvenn',
+        url: 'http://mayvenn.com'
+    },
+    {
+        name: 'NPM, inc.',
+        url: 'http://npmjs.com'
+    },
+    {
+        name: 'Pandora',
+        url: 'http://pandora.com'
+    },
+    {
+        name: 'Prime',
+        url: 'http://stayinyourprime.com'
+    },
+    {
+        name: 'Rockbot',
+        url: 'http://rockbot.com'
+    },
+    {
+        name: 'ShopPad',
+        url: 'http://theshoppad.com'
+    },
+    {
+        name: 'Sungevity',
+        url: 'http://Sungevity'
+    }
+]
